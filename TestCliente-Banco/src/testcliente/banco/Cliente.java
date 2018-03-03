e7bd4c54d3b6a5cf3b66bba8e993ba8e598302b5
Unknown <yaki3011@gmail.com>
2018-03-03 10:44:25 -0300

    Creando Clases

---
+++ TestCliente-Banco/src/testcliente/banco/Cliente.java
@@ -0,0 +1,41 @@
+
+package testcliente.banco;
+
+/**
+ *
+ * @author Yaki's
+ */
+public class Cliente {
+
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return this.apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
+}
---
+++ TestCliente-Banco/src/testcliente/banco/TestClienteBanco.java
@@ -0,0 +1,17 @@
+
+package testcliente.banco;
+
+/**
+ *
+ * @author Yaki's
+ */
+public class TestClienteBanco {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
